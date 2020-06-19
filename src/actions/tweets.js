a238c9429930ee97ce33fda8de38830181ab2a4e
swethusiast <aitc.dev@gmail.com>
2020-06-19 22:59:53 +0200

    finished!

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,6 +1,12 @@
-import { saveLikeToggle } from '../utils/api';
+import { saveLikeToggle, saveTweet } from '../utils/api';
+import { showLoading, hideLoading } from 'react-redux-loading';
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS';
 export const TOGGLE_TWEET = 'TOGGLE_TWEET';
+export const ADD_TWEET = 'ADD_TWEET';
+export const addTweet = (tweet) => ({
+    type: ADD_TWEET,
+    tweet,
+});
 
 export const receiveTweets = (tweets) => ({
     type: RECEIVE_TWEETS,
@@ -21,4 +27,13 @@ export const handleToggleTweet = (info) => (dispatch) => {
         dispatch(toggleTweet(info));
         alert('There was an error liking the tweet. Try again.');
     });
+};
+export const handleAddTweet = (text, replyingTo) => (dispatch, getState) => {
+    const { authedUser } = getState();
+    dispatch(showLoading());
+    return saveTweet({ text, author: authedUser, replyingTo })
+        .then((tweet) => {
+            dispatch(addTweet(tweet));
+        })
+        .then(() => dispatch(hideLoading()));
 };
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,6 +5,7 @@ import { TiArrowBackOutline } from 'react-icons/ti/index';
 import { TiHeartOutline } from 'react-icons/ti/index';
 import { TiHeartFullOutline } from 'react-icons/ti/index';
 import { handleToggleTweet } from '../actions/tweets';
+import { Link, withRouter } from 'react-router-dom';
 
 class Tweet extends Component {
     handleLike = (e) => {
@@ -16,15 +17,16 @@ class Tweet extends Component {
     toParent = (e, id) => {
         e.preventDefault();
         // todo: Redirect to parent Tweet.
+        this.props.history.push(`/tweet/${id}`);
     };
     render() {
         const { tweet } = this.props;
         if (tweet === null) {
             return <p>This tweet doesn't exist</p>;
         }
-        const { name, avatar, timestamp, text, hasLiked, likes, replies, parent } = tweet;
+        const { name, avatar, timestamp, text, hasLiked, id, likes, replies, parent } = tweet;
         return (
-            <div className="tweet">
+            <Link to={`/tweet/${id}`} className="tweet">
                 <img src={avatar} alt={`Avatar of ${name}`} className="avatar" />
                 <div className="tweet-info">
                     <div>
@@ -50,7 +52,7 @@ class Tweet extends Component {
                         <span>{likes !== 0 && likes}</span>
                     </div>
                 </div>
-            </div>
+            </Link>
         );
     }
 }
@@ -62,4 +64,4 @@ function mapStateToProps({ authedUser, users, tweets }, { id }) {
         tweet: tweet ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet) : null,
     };
 }
-export default connect(mapStateToProps)(Tweet);
+export default withRouter(connect(mapStateToProps)(Tweet));
